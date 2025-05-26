d72fbfab28ef18be6de01767171c176431cd42c1
found-rahat <foundrahat1999@gmail.com>
2025-05-26 13:12:54 +0600

    make an user panal info

---
+++ app/Http/Controllers/UserPanalInfoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserPanalInfoController extends Controller
+{
+    // 
+}
---
+++ database/migrations/2025_05_26_125254_create_user_panal_infos_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_panal_infos', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('user_panal');
+
+            $table->string('user_refer_by')->nullable();
+            $table->integer('total_user_balance')->default(0);
+            $table->integer('total_user_paid')->default(0);
+            $table->integer('total_user_Pre_Payment')->default(0);
+            $table->integer('total_user_diposit')->default(0);
+            $table->integer('total_user_Withdrew')->default(0);
+            $table->integer('total_user_upload')->default(0);
+            $table->integer('total_user_order')->default(0);
+
+            $table->string('user_phone_numebr')->nullable();
+            $table->string('profile_images')->nullable();
+            $table->string('user_facebook_URL')->nullable();
+            $table->string('user_ecommerce_url')->nullable();
+            $table->string('user_last_login')->nullable();
+            $table->string('user_last_ip_adress')->nullable();
+            $table->string('user_payment_status')->default('unverified');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_panal_infos');
+    }
+};
