cb2018cc7f192ec154f2d66fc0c7856e917af25f
found-rahat <foundrahat1999@gmail.com>
2025-05-26 13:13:30 +0600

    make an order list

--- database/migrations/2025_05_26_101349_create_order_lists_table.php
+++ database/migrations/2025_05_26_101349_create_order_lists_table.php
@@ -13,6 +13,29 @@ return new class extends Migration
     {
         Schema::create('order_lists', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('user_panal');
+
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->foreign('product_id')->references('id')->on('admin_product');
+
+            $table->unsignedBigInteger('customer_info_id')->nullable();
+            $table->foreign('customer_info_id')->references('id')->on('customer_info');
+
+            $table->string('order_number');
+
+            $table->string('order_create_time');
+
+            $table->integer('item_price'); //admin price
+
+            $table->string('verified_status')->default('check');
+
+            $table->integer('item_quentity'); //2
+            $table->integer('unit_price'); //300
+            $table->integer('paid_price'); //300*2 =600
+
+            $table->string('status')->default('Pending');
             $table->timestamps();
         });
     }
