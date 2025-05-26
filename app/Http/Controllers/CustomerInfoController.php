bec751048d30da72b22ed3b5e40333e63fbdedb3
found-rahat <foundrahat1999@gmail.com>
2025-05-26 16:56:13 +0600

    customer info insert done

--- app/Http/Controllers/CustomerInfoController.php
+++ app/Http/Controllers/CustomerInfoController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AddCart;
+use App\Models\CuriorServiceProviderCost;
+use App\Models\CustomerInfo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerInfoController extends Controller
 {
-    // 
+   
+      public function customerinsert(Request $request) {
+        $user = Auth::user();
+        $grandTotal = 0;
+        $grandqty = 0 ;
+        
+        $cartItems = AddCart::with('product')->where('user_name', $user->name)->get();
+        foreach ($cartItems as $item) {
+            $price = (float) $item->sell_price;
+            $qty = (int) $item->quentity;
+            $total = $price * $qty;
+            $grandTotal += $total;
+            $grandqty += $qty;
+        }
+
+        $curior = CuriorServiceProviderCost::where('title',$request->curior)->get();
+        foreach ($curior as $cost) {
+            $amount = $cost->amount;
+            $provider = $cost->name;
+        }
+
+
+        $lastOrder = CustomerInfo::orderBy('order_number', 'desc')->first();
+        if ($lastOrder && preg_match('/^Ds(\d+)$/', $lastOrder->order_number, $matches)) {
+            $nextNumber = intval($matches[1]) + 125;
+        } else{
+            $nextNumber = 1; // Start from 1 if no previous order
+        }
+
+        // Format to string like Ds0001
+        $orderNumber = 'Ds' . str_pad($nextNumber, 5, '0', STR_PAD_LEFT);
+                
+
+
+        CustomerInfo::insert([
+            'user_id' => $user->id,
+            'order_number' => $orderNumber,
+            'name' => $request->name,
+            'address' => $request->address,
+            'phone' => $request->phone_number,
+            'item_quentity' => $grandqty,
+            'total_paid' => $grandTotal,
+            'order_create_time' => now(),
+            'shipping_fee' => $amount,
+            'shipping_provider' => $provider,
+            'order_note' => $request->note,
+        ]);
+        return back()->with('success', 'Order submitted successfully!');
+
+    }
 }
--- app/Models/CustomerInfo.php
+++ app/Models/CustomerInfo.php
@@ -2,10 +2,51 @@
 
 namespace App\Models;
 
+use App\Models\UserPanal;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CustomerInfo extends Model
 {
     use HasFactory;
+    protected $table = 'customer_infos';
+    protected $fillable =[
+        'user_id',
+        'order_number',
+        'name',
+        'address',
+        'phone',
+        'item_quentity',
+        'total_paid',
+        'order_create_time',
+        'discount',
+        'discount_user',
+        'payment_method',
+        'payment_status',
+        'confirm_time',
+        'confirm_user',
+        'shipping_fee',
+        'shipping_provider',
+        'shipping_provider_delivery_code',
+        'shipped_type',
+        'shipped_time',
+        'shipped_user',
+        'shippment_id',
+        'pre_payment',
+        'pre_payment_user',
+        'order_note',
+        'hold_reason',
+        'hold_time',
+        'cancel_reason',
+        'cancel_time',
+        'delivery_time',
+        'status',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(UserPanal::class, 'user_id');
+    }
+
 }
