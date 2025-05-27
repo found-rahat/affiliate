97a558c1d5e485451ca8b59e7002daa67b76e871
found-rahat <foundrahat1999@gmail.com>
2025-05-27 13:26:13 +0600

    report form change

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -10,74 +10,67 @@
                 <h3 class="fw-bold mb-3">Report Us</h3>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="card-title">Report Form</div>
+
+        @if (Session::has('success'))
+            <div class="alert alert-success">{{ Session::get('success') }}</div>
+        @endif
+
+        <form action="{{ route('report.authenticate') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="card p-4">
+                <h4 class="card-title mb-3">Report Details</h4>
+
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="Curiorservice" class="form-label">Report Type</label>
+                        <select name="type" class="form-control form-select" id="largeSelect">
+                            <option>---Selection Type---</option>
+                            <option value="Product Problem">Product Problem</option>
+                            <option value="Payment Issue">Payment Issue</option>
+                            <option value="Delivery Problem">Delivery Problem</option>
+                            <option value="Other">Other</option>
+                        </select>
                     </div>
-                    @if (Session::has('success'))
-                        <div class="alert alert-success">{{ Session::get('success') }}</div>
-                    @endif
-                    <form action="{{ route('report.authenticate') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="container">
-                            <div class="">
-                                <div style="float: left" class="form-group col-md-6 col-lg-6">
-                                    <label for="largeSelect">Type</label>
-                                    <select name="type" class="form-select form-control-lg" id="largeSelect">
-                                        <option>---Selection Type---</option>
-                                        <option value="Product Problem">Product Problem</option>
-                                        <option value="Payment Issue">Payment Issue</option>
-                                        <option value="Delivery Problem">Delivery Problem</option>
-                                        <option value="Other">Other</option>
-                                    </select>
 
-                                </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="text" class="form-label">Report Subject</label>
 
-                                <div style="float: left" class="form-group col-md-6 col-lg-6">
-                                    <label for="text">Subject</label>
-                                    <input value="{{ old('subject') }}" type="text"
-                                        class="form-control @error('subject') is-invalid @enderror" id="text"
-                                        name="subject" placeholder="Enter Subject" />
-                                    @error('subject')
-                                        <p class="invalid-feedback">{{ $message }}</p>
-                                    @enderror
-                                </div>
-
-                                <div class="form-group col-md-12">
-                                    {{-- <label for="exampleFormControlTextarea1">Description</label> --}}
-                                    <textarea value="{{ old('subject') }}" name="description"
-                                        class="form-control @error('description') is-invalid @enderror" id="exampleFormControlTextarea1"
-                                        placeholder="Leave a Description here" rows="3"></textarea>
-                                    @error('description')
-                                        <p class="invalid-feedback">{{ $message }}</p>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleFormControlFile1">(Optional) Example file input</label>
-                                    <input name="attachment[]" type="file" multiple class="form-control-file"
-                                        id="exampleFormControlFile1" />
-                                </div>
-                                <div class="form-group">
-                                    <button class="btn btn-info"> <span class="btn-label"> <i
-                                                class="fa fa-exclamation-circle"></i> Report </button>
-                                </div>
-
-                            </div>
-                        </div>
-                    </form>
+                        <input value="{{ old('subject') }}" type="text"
+                            class="form-control @error('subject') is-invalid @enderror" id="text" name="subject"
+                            placeholder="Enter Subject" />
+                        @error('subject')
+                            <p class="invalid-feedback">{{ $message }}</p>
+                        @enderror
+                    </div>
 
                 </div>
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="address" class="form-label">Report Details </label>
+                        <textarea value="{{ old('subject') }}" name="description"
+                            class="form-control @error('description') is-invalid @enderror" id="exampleFormControlTextarea1"
+                            placeholder="Leave a Description here" rows="3"></textarea>
+                        @error('description')
+                            <p class="invalid-feedback">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="exampleFormControlFile1">(Optional) Example file input</label>
+                        <input name="attachment[]" type="file" multiple class="form-control-file"
+                            id="exampleFormControlFile1" />
+                    </div>
+                </div>
+                <button class="btn btn-warning"> <span class="btn-label"> <i class="fa fa-exclamation-circle"></i> Report
+                </button>
             </div>
-        </div>
+        </form>
 
-        <div class="card">
+        <div class="card alert-black">
             <div class="card-header">
                 <div class="card-title">Reports data</div>
             </div>
-            <div class="card-body">
-                <table class="table table-head-bg-primary">
+            <div class="table-responsive">
+                <table class="display table table-striped table-hover">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -87,7 +80,7 @@
                             <th scope="col">Attachemnt</th>
                             <th scope="col">Note</th>
                             <th scope="col">Status</th>
-                            <th scope="col"></th>
+                            <th scope="col">View</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -98,14 +91,15 @@
                                 <td>{{ $report->subject }}</td>
                                 <td>{{ Str::limit($report->description, 10) }}</td>
                                 <td>
-                                    @if(!empty($report->attachment))
+                                    @if (!empty($report->attachment))
                                         @php
                                             $images = json_decode($report->attachment, true);
                                         @endphp
 
-                                        @if(is_array($images))
+                                        @if (is_array($images))
                                             @foreach ($images as $img)
-                                                <img src="{{ asset('storage/' . $img) }}" width="100" height="100" alt="Attachment">
+                                                <img src="{{ asset('storage/' . $img) }}" width="100" height="100"
+                                                    alt="Attachment">
                                             @endforeach
                                         @else
                                             <p>No images found.</p>
@@ -113,11 +107,12 @@
                                     @else
                                         <p>No attachments available.</p>
                                     @endif
-                                    </td>
+                                </td>
                                 <td>{!! Str::limit($report->admin_note, 10) !!}</td>
                                 <td>{{ $report->status }}</td>
                                 <td>
-                                    <a href="{{ route('user.reportsview', ['id' => $report->id]) }}" class="btn btn-info">View</a>
+                                    <a href="{{ route('user.reportsview', ['id' => $report->id]) }}"
+                                        class="btn btn-info">View</a>
 
 
                                 </td>
@@ -139,7 +134,7 @@
     <script>
         setTimeout(function() {
             location.reload();
-        }, 3000); // Reloads after 3 seconds
+        }, 1000); // Reloads after 3 seconds
     </script>
 @endif
 @include('include.footer')
